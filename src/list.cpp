4b5c12ab4f80f4c42d61a400a293d8a849281b94
Luther History <whoismrhistory@gmail.com>
2026-09-12 22:56:41 +0630

    Saturday, September 12, 2026 #3

--- src/list.cpp
+++ src/list.cpp
@@ -1,6 +1,8 @@
-#include <cstring>
+#include <exception>
 #include <iostream>
 #include <filesystem>
+#include <vector>
+#include <algorithm>
 
 #define RESET 	"\033[0m"
 
@@ -15,60 +17,128 @@
 
 namespace fs = std::filesystem;
 
-bool is_image(const std::string& name) {
-	const char* img_ext[] = {
-		".png", ".jpeg", ".gif",
-		".jpg", ".webp", ".avif",
-		".svg", ".eps",
-	};
+void output_lists(fs::path& path) {
+	std::vector<fs::directory_entry> entries;
 
-	for (auto& e : img_ext)
-		if (name == e)
-			return true;
+	for (const auto& entry : fs::directory_iterator(path)) {
+		
+		entries.push_back(entry);
+	}
 
-	return false;
-}
+	std::sort(
+		entries.begin(),
+		entries.end(),
+		[](const auto& a, const auto& b){
+			return (
+				a.path().filename().string()
+				<
+				b.path().filename().string()
+			);
+		}
+	);
 
-int main(int argc, char **argv) {
-	fs::path current = ".";
-
-	for (const auto& entry : fs::directory_iterator(current)) {
-		auto item = entry.path().filename();
+	for (const auto& entry : entries) {
+		char sign = ' ';
 
 		if (entry.is_directory()) {
 			std::cout << BRIGHT << BLUE;
-			item.concat(RESET "/");
-		}
-
-		else if (entry.is_symlink()) {
-			std::cout << BRIGHT << CYAN;
-			item.concat(RESET "@");
+			sign = '/';
 		}
 
 		else if (entry.is_fifo()) {
 			std::cout << YELLOW;
-			item.concat(RESET "|");
+			sign = '|';
 		}
 
-		else if (entry.is_socket()) {
-			std::cout << BRIGHT << YELLOW;
-			item.concat(RESET);
+		else if (entry.is_symlink()) {
+			std::cout << BRIGHT << CYAN;
+			sign = '@';
 		}
 
 		else if (entry.is_regular_file()) {
-			if (is_image(item.extension().c_str())) {
-				std::cout << RED;
-				item.concat(RESET "^");
+			if (entry.is_socket()) {
+				std::cout << BRIGHT << MAGENTA;
 			}
 
-			else if (false) {
+			else if (entry.is_block_file() || entry.is_character_file()) {
+				std::cout << BRIGHT << YELLOW;
+			}
 
+			else if (entry.is_other()) {
+				std::cout << RED;
+			}
+
+			sign = '\0';
+
+			if ((entry.status().permissions() & (
+				fs::perms::owner_exec |
+				fs::perms::group_exec |
+				fs::perms::others_exec
+				)) != fs::perms::none
+			) {
+				std::cout << BRIGHT << GREEN;
+				sign = '*';
 			}
 		}
 
-		std::cout << item.c_str() << "  ";
+		std::cout 
+			<< entry.path()
+					.filename()
+					.string();
+
+		std::cout << RESET;
+		std::cout << sign << "  ";
 	}
 	std::cout << std::endl;
+}
 
-	return 0;
+int main(int argc, char **argv) {
+	int status = 0;
+
+	try {
+		std::vector<fs::path> list;
+		
+		for (int i=(argc > 1); i < argc; i++) {
+			std::string_view temp = (
+				i
+				?
+				argv[i]
+				:
+				"."
+			);
+
+			if (!fs::exists(temp)) {
+				std::cerr 
+					<< argv[0]
+					<< ": cannot access '" 
+					<< argv[i]
+					<< "': No such file or directory"
+					<< std::endl;
+				
+				status = 2;
+			}
+	
+			else {
+				list.push_back(temp);
+			}
+		}
+	
+		for (auto& ls : list) {
+			if (argc > 2) {
+				std::cout << std::endl;
+				std::cout << ls.c_str() << ":\n";
+			}
+			output_lists(ls);
+		}
+	} 
+
+	catch (const std::exception& e) {
+		std::cerr << e.what() << std::endl;
+	}
+
+	catch (...) {
+		std::cerr << 0[argv] << ": error" << std::endl;
+	}
+
+	return status;
 }
